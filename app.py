b60e1dccedac08cdc326e4f32fe868722d6ee30d
Habeey <Habyndiaye209@gmail.com>
2025-03-20 14:22:03 +0100

    push de l'app streamlit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-import streamlit as st
-import pickle
-import numpy as np
+# --------- STREAMLIT ---------
 
-# Charger le modèle et le vectorizer
+import streamlit as st
+import joblib
+
+# Charger le modèle et le vectorizer avec joblib
 with open("model_lr.pkl", "rb") as model_file:
-    model_lr = pickle.load(model_file)
+    model_lr = joblib.load(model_file)
 with open("tfidf_vectorizer.pkl", "rb") as vectorizer_file:
-    tfidf_vectorizer = pickle.load(vectorizer_file)
+    tfidf_vectorizer = joblib.load(vectorizer_file)
 
 # Interface Streamlit
 st.title("Analyse de Sentiment des Avis de Films")
@@ -24,7 +25,7 @@ if st.button("Prédire"):
         prediction = model_lr.predict(user_input_tfidf)[0]
         
         # Affichage du résultat
-        sentiment = "Positif" if prediction == 1 else "Négatif"
+        sentiment = "Positif" if prediction == "Positif" else "Négatif"
         st.write(f"### Sentiment prédit : {sentiment}")
     else:
         st.warning("Veuillez entrer un texte avant de prédire.")
